0f3367a4a2eb6ecaf035ca6600659d3f8009ae42
Abraham Cabrera <ingeabraham23@gmail.com>
2024-01-26 14:04:30 -0600

    agregado boton borrar datos

--- src/components/RespaldoYRestauracion.jsx
+++ src/components/RespaldoYRestauracion.jsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import db from "../db";
 import jsonData from "./backupFondoDeAhorro.json";
 import "./RespaldoYRestauracion.css"
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function RespaldoYRestauracion() {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -26,9 +28,9 @@ function RespaldoYRestauracion() {
       a.download = "backupFondoDeAhorro.json";
       a.click();
 
-      console.log("Base de datos respaldada correctamente");
+      toast.success("Base de datos respaldada correctamente");
     } catch (error) {
-      console.error("Error al respaldar la base de datos:", error);
+      toast.error("Error al respaldar la base de datos:", error);
     }
   };
 
@@ -46,10 +48,25 @@ function RespaldoYRestauracion() {
         // Insertar los nuevos registros desde el archivo JSON
         await db.personas.bulkAdd(parsedData);
 
-        console.log("Base de datos restaurada correctamente");
+        toast.success("Base de datos restaurada correctamente");
       }
     } catch (error) {
-      console.error("Error al restaurar la base de datos:", error);
+      toast.error("Error al restaurar la base de datos:", error);
+    }
+  };
+
+  const limpiarDatos = async () => {
+    try {
+      // Abre la conexión a la base de datos
+      await db.open();
+
+        // Borrar todos los registros actuales antes de restaurar
+        await db.personas.clear();
+
+        toast.success("Datos borrados exitosamemte");
+      
+    } catch (error) {
+      toast.error("Error al borrar los datos:", error);
     }
   };
 
@@ -64,9 +81,9 @@ function RespaldoYRestauracion() {
       // Insertar los nuevos registros desde los datos importados del archivo JSON
       await db.personas.bulkAdd(jsonData);
 
-      console.log("Datos cargados desde JSON correctamente");
+      toast.success("Datos cargados desde JSON correctamente");
     } catch (error) {
-      console.error("Error al cargar datos desde JSON:", error);
+      toast.error("Error al cargar datos desde JSON:", error);
     }
   };
 
@@ -85,10 +102,11 @@ function RespaldoYRestauracion() {
       </div>
       <div className="button-container">
         <button className="button backup" onClick={handleBackup}>Crear respaldo</button>
-        
-        
       </div>
       <div className="button-container"><button className="button load" onClick={handleLoadFromJSON}>Cargar datos guardados anteriormente</button></div>
+      <div className="button-container">
+        <button className="button-delete" onClick={limpiarDatos}>Borrar datos</button>
+      </div>
     </div>
   );
 }
